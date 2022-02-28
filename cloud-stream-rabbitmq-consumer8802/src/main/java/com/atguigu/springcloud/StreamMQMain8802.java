c07fc4456d2fc3642a5c03ebdca366db30dca9d5
JionLin <250912986@qq.com>
2022-02-28 17:24:17 +0800

    springcloud stream 接收问题 1.pom问题引入rabbitmq, 2.yml文件配置 eureka问题,和rabbitmq整合的账号/密码,属于接收信息 input，消息类型,消息的名称定义,消息服务的具体设置 3.接收消息后进行添加注解 enablebinding(sink.class) 4.进行方法接收的时候,进行添加注解streamListen（Sink.input） 进行接收信息。

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQMain8802.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author johnny
+ * @Classname StreamMQMain8802
+ * @Description
+ * @Date 2022/2/28 5:06 下午
+ */
+@SpringBootApplication
+public class StreamMQMain8802
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
