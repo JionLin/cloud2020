fbcd9dd5853df40998160509a7b682ba9d5cd931
JionLin <250912986@qq.com>
2022-03-10 14:40:05 +0800

    账户组件。

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/dao/AccountDao.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.alibaba.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+
+/**
+ * @author johnny
+ * @Classname AccountDao
+ * @Description
+ * @Date 2022/3/10 2:34 下午
+ */
+@Mapper
+public interface AccountDao {
+
+    /**
+     * 扣减账户余额
+     */
+    void decrease(@Param("userId") Long userId, @Param("money") BigDecimal money);
+}
