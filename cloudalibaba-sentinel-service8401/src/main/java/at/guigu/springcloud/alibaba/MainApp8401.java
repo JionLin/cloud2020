315896ac7d9810d42c5e5cdacf6511c9276f2947
JionLin <250912986@qq.com>
2022-04-23 16:59:57 +0800

    获取sha-256 的加密形式。

--- cloudalibaba-sentinel-service8401/src/main/java/at/guigu/springcloud/alibaba/MainApp8401.java
+++ cloudalibaba-sentinel-service8401/src/main/java/at/guigu/springcloud/alibaba/MainApp8401.java
@@ -1,8 +1,17 @@
 package at.guigu.springcloud.alibaba;
 
+import com.sun.org.apache.regexp.internal.RE;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
 
 /**
  * @author johnny
@@ -10,11 +19,57 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @Description
  * @Date 2022/3/3 11:34 上午
  */
+@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MainApp8401
 {
     public static void main(String[] args) {
         SpringApplication.run(MainApp8401.class, args);
-    }
+
+//        File file=new File("/Users/johnny/Downloads/111.docx");
+//        byte[] buffer=null;
+//        try {
+//            FileInputStream fileInputStream=new FileInputStream(file);
+//            ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+//            byte[] bytes=new byte[1024];
+//            int n;
+//            while ((n= fileInputStream.read(bytes))!=-1){
+//                byteArrayOutputStream.write(bytes,0,n);
+//            }
+//            fileInputStream.close();
+//            byteArrayOutputStream.close();
+//            buffer=byteArrayOutputStream.toByteArray();
+//            System.out.println(buffer);
+//        }catch (Exception e){
+//
+//        }
+//        System.out.println(getsha(buffer));
+//
+//
+//    }
+//
+//
+//
+//
+//    public static String  getsha(byte[] filename ){
+//        MessageDigest messageDigest;
+//        String encdeStr="";
+//        try {
+//            messageDigest = MessageDigest.getInstance("SHA-256");
+//            ByteArrayInputStream inputStream = new ByteArrayInputStream(filename);
+//            byte[] buffer = new byte[1024];
+//            int len =0;
+//            while ((len=inputStream.read(buffer))>0){
+//                messageDigest.update(buffer,0,len);
+//            }
+//            inputStream.close();
+//            byte[] hash =messageDigest.digest();
+//            encdeStr= Hex.encodeHexString(hash);
+//        }catch (Exception e){
+//
+//        }
+//        return encdeStr;
+//    }
+
 }
